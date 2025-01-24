7200e17aa97656e9683c57a9d39dc6e1d439b842
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-01-24 12:26:03 +0700

    test GET request

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { devnightMembers } from "./data/devnight_members.js";
 
 const app = express();
+app.use(bodyParser.json()); // to be allowed to access the params
 
 // "/trips"    = root | endpoint | endpoint path
 // () => {...} = controller function | handler function
-app.get("/trips", (req, res) => {
+app.get("/members", (req, res) => {
   res.send("Attraction Data");
+});
+
+app.get("/members/:id", (req, res) => {
+  const memberId = Number(req.params.id);
+  const member = devnightMembers.filter((member) => {
+    return member.id === memberId;
+  });
+  console.log(memberId);
+
+  return res.json({ data: member[0] });
 });
 
 app.listen(4000, () => {
