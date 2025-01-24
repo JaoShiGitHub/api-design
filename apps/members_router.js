be5f43f7218d2028d58517c38a55b53dad7467a6
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-01-24 17:04:17 +0700

    Express Router

--- apps/members_router.js
+++ apps/members_router.js
@@ -1,8 +1,63 @@
 import { Router } from "express";
-import { devnightMembers } from "../data/devnight_members";
+import { devnightMembers } from "../data/devnight_members.js";
 
-const membersRouter = Router();
+const memberRouter = Router();
 
-membersRouter.get("/", (req, res) => {
-  res.json();
+// "/trips"    = root | endpoint | endpoint path
+// () => {...} = controller function | handler function
+memberRouter.get("/", (req, res) => {
+  // res.send("Attraction Data");
+  res.json({ data: devnightMembers });
 });
+
+memberRouter.get("/:id", (req, res) => {
+  const memberId = Number(req.params.id);
+  const member = devnightMembers.filter((member) => {
+    return member.id === memberId;
+  });
+  console.log(memberId);
+
+  return res.json({ data: member[0] });
+});
+
+memberRouter.post("/", (req, res) => {
+  const newMember = req.body;
+
+  devnightMembers.push({
+    id: devnightMembers[devnightMembers.length - 1].id + 1,
+    ...newMember,
+  });
+
+  return res.json({ message: "Member info has been created" });
+});
+
+memberRouter.put("/:id", (req, res) => {
+  const updateMember = req.body; // The data that the user needs to update
+  const memberId = req.params.id; // The member that the user needs to update
+
+  const updateMemberId = devnightMembers.findIndex(
+    (member) => member.id == memberId
+  );
+  devnightMembers[updateMemberId] = {
+    id: devnightMembers[updateMemberId].id,
+    ...updateMember,
+  };
+
+  return res.json({
+    message: "Member info has been updated",
+  });
+});
+
+memberRouter.delete("/:id", (req, res) => {
+  const deleteId = Number(req.params.id);
+  const index = devnightMembers.findIndex((member) => member.id === deleteId);
+
+  devnightMembers.splice(index, 1);
+
+  return res.json({
+    message: "The member has been deleted",
+    devnightMembers,
+  });
+});
+
+export default memberRouter;
