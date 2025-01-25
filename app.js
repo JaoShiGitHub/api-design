369d1dfd837dc9123ba85007bc89f74807dccf76
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-01-25 23:57:31 +0700

    Middleware

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(bodyParser.json()); // to be allowed to access the params
 app.use("/members", membersRouter);
 
+const logging = (req, res, next) => {
+  console.log(
+    `IP: ${req.ip}, HttpMethod: ${req.method}, Endpoint: ${req.originalUrl}`
+  );
+};
+
+app.use(logging);
+
 app.listen(4000, () => {
   console.log("Server is listening at port 4000");
 });
