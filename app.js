f1e781e3e13b74b4e552020ccaa9f5ab313ab3a5
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2025-01-26 00:07:45 +0700

    Middleware

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ import bodyParser from "body-parser";
 import membersRouter from "./apps/members_router.js";
 
 const app = express();
-app.use(bodyParser.json()); // to be allowed to access the params
-app.use("/members", membersRouter);
 
 const logging = (req, res, next) => {
   console.log(
     `IP: ${req.ip}, HttpMethod: ${req.method}, Endpoint: ${req.originalUrl}`
   );
+  next();
 };
 
 app.use(logging);
+app.use(bodyParser.json()); // to be allowed to access the params
+
+app.use("/members", membersRouter);
 
 app.listen(4000, () => {
   console.log("Server is listening at port 4000");
